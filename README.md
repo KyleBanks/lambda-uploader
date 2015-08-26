f34bfd5dd3b5b565c31484670acf5aff282a4be1
KyleBanks <kyle@nthgensoftware.com>
2015-08-25 21:32:28 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# lambda-uploader
+
+A module for uploading a source directory to [AWS Lambda](https://aws.amazon.com/lambda/) as a function. 
+
+## Installation
+```
+npm install lambda-uploader
+```
+
+## Example
+
+Assuming you have a folder called `/path/to/lambda-function` that contains the source code and dependencies of a Lambda function:
+
+```node
+var lambdaUploader = require('lambda-uploader');
+
+lambdaUploader.uploadFunction(
+
+    'my-awesome-function', // Function Name
+    '/path/to/lambda-function', // Lambda Function Source Directory
+    'index.handler', // Handler Name
+    '<Lambda Execution Role ARN>', 
+    512, // Memory in Megabytes
+    30, // Timeout in Seconds
+    
+    function(err) {
+        if (err) {
+            throw err;
+        }
+        
+        console.log("Lambda function uploaded!");
+    }
+);
+
+```
+
+## Notes
+
+- If a function exists with the name provided, the function will be updated. Otherwise, a new Lambda function will be created.
+
---
+++ index.js
@@ -0,0 +1,102 @@
+/**
+ * index.js
+ *
+ * AWS Lambda function Uploader.
+ *
+ * Created by kylewbanks on 15-02-28.
+ */
+
+/**
+ * @imports
+ */
+var AWS = require('aws-sdk');
+var async = require('async');
+var fs = require('fs');
+var archiver = require('archiver');
+
+/**
+ * @private
+ */
+var _lambda = new AWS.Lambda(
+    {
+        apiVersion: '2014-11-11'
+    }
+);
+
+/**
+ * @public
+ */
+module.exports = {
+
+    /**
+     * Uploads a source directory to AWS Lambda, either creating a new function or overwriting
+     * the function with the specified name if it already exists.
+     *
+     * @param name {String}
+     * @param directoryPath {String}
+     * @param handler {String} The name of the function handler in the format of "<filename>.<exported-function-name>"
+     *  ex: index.handler
+     * @param role {String} The ARN of the Lambda Execution Role
+     * @param memory {Number} The amount of memory, in MB, your Lambda function is given.
+     * @param timeout {Number} The function execution time at which Lambda should terminate the function, in seconds.
+     * @param cb {function(Error)}
+     */
+    uploadFunction: function(name, directoryPath, handler, role, memory, timeout, cb) {
+        async.waterfall([
+
+            /**
+             * Compress the source directory into a ZIP file.
+             *
+             * @param next {function(Error, String)}
+             */
+            function(next) {
+                var zipFilePath = 'tmp-lambda-func.zip';
+                var zipStream = fs.createWriteStream(zipFilePath);
+                var archive = archiver('zip');
+
+                zipStream.on('close', function() {
+                    next(null, zipFilePath);
+                });
+
+                archive.on('error', next);
+
+                archive.pipe(zipStream);
+                archive.directory(directoryPath, false);
+                archive.finalize();
+            },
+
+            /**
+             * Upload the zip and create the lambda function.
+             *
+             * @param zipFilePath {String}
+             * @param next {function(Error, String)}
+             */
+            function(zipFilePath, next) {
+                _lambda.uploadFunction({
+                    FunctionName: name,
+                    FunctionZip: fs.readFileSync(zipFilePath),
+                    Handler: handler,
+                    Mode: 'event',
+                    Role: role,
+                    Runtime: 'nodejs',
+                    MemorySize: memory,
+                    Timeout: timeout
+                }, function(err) {
+                    next(err, zipFilePath);
+                });
+            },
+
+            /**
+             * Delete the Zipped Lambda function.
+             *
+             * @param zipFilePath {String}
+             * @param next {function(Error)}
+             */
+            function(zipFilePath, next) {
+                fs.unlink(zipFilePath, next);
+            }
+
+        ], cb);
+    }
+
+};
